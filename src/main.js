b6f116d1abcc6797ac0cce416010eb291b90f8cb
Jay patel <jayp962001@gmail.com>
2023-12-30 17:48:29 +0530

    Upadated

--- src/main.js
+++ src/main.js
@@ -1,240 +1,182 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { KTX2Loader } from "three/examples/jsm/loaders/KTX2Loader";
+import * as dat from "dat.gui";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
-import * as dat from "dat.gui";
+import { SSAOPass } from "three/examples/jsm/postprocessing/SSAOPass.js";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(5, 5, 5);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
+scene.background = new THREE.Color(0x000000); // Set 3D scene's background color to white
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: false });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.25;
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const composer = new EffectComposer(renderer);
-const renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
+let loadedModel; // Variable to store the loaded model
 
+// Orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.dampingFactor = 0.05;
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
 
-const textureLoader = new TextureLoader();
-const dracoLoader = new DRACOLoader();
-const ktx2Loader = new KTX2Loader();
+function createMaterialFromJSON(jsonData) {
+  // Load textures
+  const diffuseMap = new THREE.TextureLoader().load(jsonData.diffuseMap);
+  const glossMap = new THREE.TextureLoader().load(jsonData.glossMap);
+  const normalMap = new THREE.TextureLoader().load(jsonData.normalMap);
 
-ktx2Loader.setTranscoderPath("/basis/");
-ktx2Loader.detectSupport(renderer);
+  // Set texture wrapping
+  diffuseMap.wrapS = THREE.RepeatWrapping;
+  diffuseMap.wrapT = THREE.RepeatWrapping;
+  glossMap.wrapS = THREE.RepeatWrapping;
+  glossMap.wrapT = THREE.RepeatWrapping;
+  normalMap.wrapS = THREE.RepeatWrapping;
+  normalMap.wrapT = THREE.RepeatWrapping;
 
-dracoLoader.setDecoderPath("/draco/");
+  // Set texture repeats
+  diffuseMap.repeat.set(...jsonData.diffuseMapTiling);
+  glossMap.repeat.set(...jsonData.glossMapTiling);
+  normalMap.repeat.set(...jsonData.normalMapTiling);
 
-const jsonContent = {
-  models: [
-    {
-      name: "Sofa",
-      glbPath: "src/Sofa.glb",
-      materials: [
-        {
-          name: "material1",
-          albedoMap: "src/albedoMap1.jpg",
-          normalMap: "src/normalMap1.jpg",
-          roughnessMap: "src/roughnessMap1.jpg",
-        },
-        {
-          name: "material2",
-          albedoMap: "src/albedoMap2.jpg",
-          normalMap: "src/normalMap2.jpg",
-          roughnessMap: "src/roughnessMap2.jpg",
-        },
-      ],
-    },
+  // Create material
+  const material = new THREE.MeshPhysicalMaterial({
+    metalness: jsonData.metalness,
+    roughness: 1 - jsonData.sheenGloss,
+    opacity: jsonData.opacity,
+    transparent: true,
+    map: diffuseMap,
+    roughnessMap: glossMap,
+    normalMap: normalMap,
+    side: jsonData.twoSidedLighting ? THREE.DoubleSide : THREE.FrontSide,
+    alphaTest: jsonData.alphaTest,
+    depthWrite: jsonData.depthWrite,
+    depthTest: jsonData.depthTest,
+    color: new THREE.Color(...jsonData.diffuse),
+    emissive: new THREE.Color(...jsonData.emissive),
+    emissiveIntensity: jsonData.emissiveIntensity,
+    aoMap: null,
+    aoMapIntensity: 1,
+  });
 
-    {
-      name: "Floor",
-      glbPath: "src/Floor.glb",
-      materials: [
-        {
-          name: "material1",
-          albedoMap: "src/albedoMap1.jpg",
-          normalMap: "src/normalMap1.jpg",
-          roughnessMap: "src/roughnessMap1.jpg",
-        },
-        {
-          name: "material2",
-          albedoMap: "src/albedoMap2.jpg",
-          normalMap: "src/normalMap2.jpg",
-          roughnessMap: "src/roughnessMap2.jpg",
-        },
-      ],
-    },
-    {
-      name: "Wall",
-      glbPath: "src/Wall.glb",
-      materials: [
-        {
-          name: "material1",
-          albedoMap: "src/albedoMap1.jpg",
-          normalMap: "src/normalMap1.jpg",
-          roughnessMap: "src/roughnessMap1.jpg",
-        },
-        {
-          name: "material2",
-          albedoMap: "src/albedoMap2.jpg",
-          normalMap: "src/normalMap2.jpg",
-          roughnessMap: "src/roughnessMap2.jpg",
-        },
-      ],
-    },
-    // Add more models here...
-    // {
-    //   name: "AnotherModel",
-    //   glbPath: "src/AnotherModel.glb",
-    //   materials: [
-    //     // Material variants for AnotherModel
-    //   ],
-    // },
-  ],
-};
+  // Additional parameters for a more realistic look
+  material.clearcoat = jsonData.clearcoat || 0; // Clearcoat intensity
+  material.clearcoatRoughness = jsonData.clearcoatRoughness || 0; // Clearcoat roughness
+  material.reflectivity = jsonData.reflectivity || 0.5; // Reflectivity
 
-// Load models
-jsonContent.models.forEach((model) => {
-  const loader = new GLTFLoader();
-  const dracoLoader = new DRACOLoader();
-  dracoLoader.setDecoderPath("/draco/");
-  loader.setDRACOLoader(dracoLoader);
+  return material;
+}
 
-  const ktx2Loader = new KTX2Loader();
-  ktx2Loader.setTranscoderPath("/basis/");
-  ktx2Loader.detectSupport(renderer);
-  loader.setKTX2Loader(ktx2Loader);
-
-  loader.load(model.glbPath, (gltf) => {
-    const materialSwitcher = {
-      materialVariant: model.materials[0].name,
-    };
+// Fetch the JSON file
+fetch("src/MaterialData/MaterialData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const jsonFiles = data;
 
     const gui = new dat.GUI();
+    const materialParameters = {
+      selectedMaterial: "material1",
+    };
+
     gui
-      .add(
-        materialSwitcher,
-        "materialVariant",
-        model.materials.map((m) => m.name)
-      )
-      .onChange((value) => {
-        const currentMaterialVariant = model.materials.find(
-          (m) => m.name === value
-        );
-        applyMaterialVariant(gltf.scene, currentMaterialVariant);
+      .add(materialParameters, "selectedMaterial", Object.keys(jsonFiles))
+      .onChange(function (value) {
+        const selectedJsonData = jsonFiles[value];
+        const newMaterial = createMaterialFromJSON(selectedJsonData);
+
+        // Update materials of the loaded model
+        if (loadedModel) {
+          loadedModel.traverse((node) => {
+            if (node.isMesh) {
+              node.material = newMaterial;
+            }
+          });
+        }
       });
 
-    applyMaterialVariant(gltf.scene, model.materials[0]);
-    scene.add(gltf.scene);
+    // Load GLB model
+    const loader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader(); // Create an instance of DRACOLoader
+    dracoLoader.setDecoderPath(
+      "https://www.gstatic.com/draco/versioned/decoders/1.4.2/"
+    ); // Set the path to the Draco decoder
 
-    // Additional setup for each loaded model...
-  });
-});
+    loader.setDRACOLoader(dracoLoader); // Set DRACOLoader to GLTFLoader
 
-window.addEventListener("resize", () => {
-  const newWidth = window.innerWidth;
-  const newHeight = window.innerHeight;
+    loader.load("src/Sofa1.glb", (gltf) => {
+      // Store the loaded model
+      loadedModel = gltf.scene;
 
-  camera.aspect = newWidth / newHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(newWidth, newHeight);
-});
-
-function applyMaterialVariant(scene, materialEntry) {
-  scene.traverse((child) => {
-    if (child.isMesh) {
-      const mesh = child;
-
-      const materials = Array.isArray(mesh.material)
-        ? mesh.material
-        : [mesh.material];
-
-      for (const material of materials) {
-        if (material instanceof THREE.MeshStandardMaterial) {
-          if (materialEntry.albedoMap) {
-            textureLoader.load(materialEntry.albedoMap, (albedoMap) => {
-              material.map = albedoMap;
-              material.needsUpdate = true;
-            });
-          } else {
-            material.map = null;
-            material.needsUpdate = true;
-          }
-
-          if (materialEntry.normalMap) {
-            textureLoader.load(materialEntry.normalMap, (normalMap) => {
-              material.normalMap = normalMap;
-              material.needsUpdate = true;
-            });
-          } else {
-            material.normalMap = null;
-            material.needsUpdate = true;
-          }
-
-          if (materialEntry.roughnessMap) {
-            textureLoader.load(materialEntry.roughnessMap, (roughnessMap) => {
-              material.roughnessMap = roughnessMap;
-              material.needsUpdate = true;
-            });
-          } else {
-            material.roughnessMap = null;
-            material.needsUpdate = true;
-          }
+      // Set initial material
+      const initialMaterial = createMaterialFromJSON(
+        jsonFiles[materialParameters.selectedMaterial]
+      );
+      loadedModel.traverse((node) => {
+        if (node.isMesh) {
+          node.material = initialMaterial;
         }
-      }
+      });
+
+      // Add the loaded model to the scene
+      scene.add(loadedModel);
+    });
+
+    camera.position.z = 5;
+
+    // Function to add HDRI
+    function setupHDRI() {
+      const rgbeloader = new RGBELoader();
+      rgbeloader.load("src/gem_2.hdr", (hdri) => {
+        const myhdr = hdri;
+        myhdr.mapping = THREE.EquirectangularReflectionMapping;
+        scene.environment = myhdr;
+        // scene.background = new THREE.Color("#000");
+      });
     }
-  });
-}
 
-// Add lights to the scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+    setupHDRI();
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 10, 5);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+    //Anti Aliasing
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
 
-// Set up shadow properties for the directional light
-directionalLight.shadow.mapSize.width = 1024;
-directionalLight.shadow.mapSize.height = 1024;
-directionalLight.shadow.camera.near = 0.1;
-directionalLight.shadow.camera.far = 50;
-directionalLight.shadow.camera.left = -10;
-directionalLight.shadow.camera.right = 10;
-directionalLight.shadow.camera.top = 10;
-directionalLight.shadow.camera.bottom = -10;
+    // SSAO pass
+    const ssaoPass = new SSAOPass(
+      scene,
+      camera,
+      window.innerWidth,
+      window.innerHeight
+    );
+    ssaoPass.kernelRadius = 16;
+    ssaoPass.minDistance = 0.01;
+    ssaoPass.maxDistance = 0.05;
+    composer.addPass(ssaoPass);
 
-// Ground plane
-const groundGeometry = new THREE.PlaneGeometry(20, 20);
-const groundMaterial = new THREE.ShadowMaterial({ opacity: 0.5 });
-const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-ground.rotation.x = -Math.PI / 2;
-ground.position.y = -2;
-ground.receiveShadow = true;
-scene.add(ground);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  composer.render();
-}
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(5, 5, 5);
+    scene.add(directionalLight);
 
-animate();
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+
+      composer.render();
+    }
+
+    animate();
+  })
+  .catch((error) => console.error("Error loading JSON file:", error));
